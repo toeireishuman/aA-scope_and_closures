9ea12cb6c175d87066e1f87ecc856fab1cfb3f0d
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-14 07:44:55 +0800

    Add my solution to 12-volume

--- practice-for-week-02-scope-problems-without-context-main/problems/12-volume.js
+++ practice-for-week-02-scope-problems-without-context-main/problems/12-volume.js
@@ -23,7 +23,29 @@ console.log(table2(1)); // prints 6
 console.log(table2(75)); // STILL prints 6
 
 ***********************************************************************/
-function recVolume(height) {}
+function recVolume(height) {
+	const dimensions = [height];
+
+	function _curriedVolume(dimension) {
+		dimensions.push(dimension);
+		if (dimensions.length >= 3) {
+			const threeDimensions = dimensions.slice(0, 3);
+			return threeDimensions.reduce(
+				(accum, current) => accum * current,
+				1
+			);
+		} else if (dimensions.length < 3) {
+			return _curriedVolume;
+		}
+	}
+
+	return _curriedVolume;
+}
+
+let table1 = recVolume(5); // returns a function
+table1(4); // returns a function
+console.log(table1(3)); // prints 60
+console.log(table1(145)); // STILL prints 60
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
