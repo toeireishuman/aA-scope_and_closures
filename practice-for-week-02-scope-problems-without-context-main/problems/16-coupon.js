8bd2d18e7240c7535e8167c95f61d4d9f593c160
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-14 07:18:09 +0800

    Add my solution to 16-coupon

--- practice-for-week-02-scope-problems-without-context-main/problems/16-coupon.js
+++ practice-for-week-02-scope-problems-without-context-main/problems/16-coupon.js
@@ -15,12 +15,12 @@ console.log(twentyPercent([10, 20, 30])); // [ 8, 16, 24 ]
 
 ***********************************************************************/
 function coupon(discount) {
-  // Your code here
+	return array => array.map(number => number * (1 - discount));
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
-  module.exports = coupon;
+	module.exports = coupon;
 } catch (e) {
-  return null;
+	return null;
 }
